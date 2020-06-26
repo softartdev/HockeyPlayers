128ed392fb176edba721f3c52806c9ee5a5499d9
artur <artik222012@gmail.com>
2020-06-26 21:37:54 +0500

    copy xcode project

---
+++ iosApp/iosApp/PlayersViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PlayersViewController.swift
+//  iosApp
+//
+//  Copyright © 2019 Square, Inc.. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import shared
+
+class PlayersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var playerData:[ForTeam] = []
+    var teamId:Int64 = -1
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        playerData = PlayerData().players(teamId: teamId)
+    }
+    
+    func showPlayers() {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return playerData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "playerRow", for: indexPath) as! PlayerRow
+        
+        let player = playerData[indexPath.row]
+        cell.fillName(name: "\(player.first_name) \(player.last_name)")
+        cell.fillNumber(number: player.number)
+        cell.fillTeamName(teamName: player.teamName)
+        
+        cell.layer.isOpaque = true
+        
+        return cell
+    }
+}
---
+++ iosApp/iosApp/TeamsViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TeamsViewController.swift
+//  iosApp
+//
+//  Copyright © 2019 Square, Inc.. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import shared
+
+class TeamsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var teamData:[Team] = TeamData().teams()
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func showTeams(teamData:TeamData) {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let team = teamData[indexPath.row]
+        
+        performSegue(withIdentifier: "ShowPlayers", sender: team)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowPlayers" {
+            let detailViewController = segue.destination as! PlayersViewController
+            let team = sender as! Team
+            detailViewController.teamId = team.id
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teamData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teamRow", for: indexPath) as! TeamRow
+        let team = teamData[indexPath.row]
+        cell.fillName(name: team.name)
+        cell.fillCoach(coach: team.coach)
+        cell.fillFounded(founded: DateFormatHelperKt.defaultFormatter.format(d: team.founded))
+        
+        cell.layer.isOpaque = true
+        
+        return cell
+    }
+}
